5036690ce221cf7b71343163b306356e8f600837
lukebelliveau <lbellive@thoughtworks.com>
2017-06-14 11:59:10 -0500

    Added legal floating point constraint

--- Calculator/App.js
+++ Calculator/App.js
@@ -1,80 +1,69 @@
 import React from 'react';
-import { Alert, StyleSheet, Text, Button, TouchableHighlight, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import Keypad from './src/Keypad';
+
+const initialState = {
+  displayText: '',
+  pendingBinaryOperation: null,
+  accumulator: 0,
+  isFloatingPoint: false,
+};
 
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      displayText: '',
-      pendingBinaryOperation: null,
-      accumulator: 0,
-    };
+    this.state = initialState;
 
     this.enterConstant = this.enterConstant.bind(this);
     this.binaryOperation = this.binaryOperation.bind(this);
+    this.enterFloatingPoint = this.enterFloatingPoint.bind(this);
     this.clear = this.clear.bind(this);
 
-    this.calculatorButtons = [
-      [
-        { value: '+', action: this.binaryOperation },
-        { value: 7, action: this.enterConstant },
-        { value: 8, action: this.enterConstant },
-        { value: 9, action: this.enterConstant },
-      ],
-      [
-        { value: '-', action: this.binaryOperation },
-        { value: 4, action: this.enterConstant },
-        { value: 5, action: this.enterConstant },
-        { value: 6, action: this.enterConstant },
-      ],
-      [
-        { value: '*', action: this.binaryOperation },
-        { value: 1, action: this.enterConstant },
-        { value: 2, action: this.enterConstant },
-        { value: 3, action: this.enterConstant },
-      ],
-      [
-        { value: '/', action: this.binaryOperation },
-        { value: 'C', action: this.clear },
-        { value: '0', action: this.enterConstant },
-        { value: '=', action: this.binaryOperation },
-      ],
-    ];
+    this.calculatorButtons = getButtons(this.enterConstant, this.enterFloatingPoint, this.binaryOperation)
   }
 
   clear() {
-    this.setState({
-      displayText: '',
-      pendingBinaryOperation: null,
-      accumulator: 0,
-    })
+    this.setState(initialState)
+  }
+
+  enterFloatingPoint() {
+    console.log(this.state);
+    if(this.state.isFloatingPoint) return;
+    this.setState((prevState) => ({
+      isFloatingPoint: true,
+      displayText: prevState.displayText + '.',
+    }));
   }
 
   enterConstant(constant) {
     this.setState((prevState) => ({
-      displayText: prevState.pendingBinaryOperation ? constant : prevState.displayText + constant.toString()
+      displayText: prevState.displayText + constant.toString()
     }))
   }
 
   binaryOperation(operator) {
+    this.setState({
+      isFloatingPoint: false,
+    });
     switch(operator) {
       case '+':
       case '-':
       case '*':
       case '/':
         this.setState((prevState) => {
-          const valueOfInput = parseInt(prevState.displayText);
+          const valueOfInput = parseFloat(prevState.displayText);
           return {
             pendingBinaryOperation: (op2) => operations[operator](valueOfInput, op2),
             accumulator: valueOfInput,
+            displayText: '',
           }
         });
         break;
       case '=':
+        if(!(this.state.pendingBinaryOperation)) break;
         this.setState((prevState) => ({
-          displayText: prevState.pendingBinaryOperation(parseInt(prevState.displayText)),
+          displayText: prevState.pendingBinaryOperation(parseFloat(prevState.displayText)),
           pendingBinaryOperation: null,
         }));
         break;
@@ -118,3 +107,36 @@ const styles = StyleSheet.create({
     flex: 3,
   }
 });
+
+const getButtons = (enterConstant, enterFloatingPoint, binaryOp, clear) => [
+  [
+    { value: '--', action: () => {} },
+    { value: '---', action: () => {} },
+    { value: '----', action: () => {} },
+    { value: '.', action: enterFloatingPoint },
+  ],
+  [
+    { value: '+', action: binaryOp },
+    { value: 7, action: enterConstant },
+    { value: 8, action: enterConstant },
+    { value: 9, action: enterConstant },
+  ],
+  [
+    { value: '-', action: binaryOp },
+    { value: 4, action: enterConstant },
+    { value: 5, action: enterConstant },
+    { value: 6, action: enterConstant },
+  ],
+  [
+    { value: '*', action: binaryOp },
+    { value: 1, action: enterConstant },
+    { value: 2, action: enterConstant },
+    { value: 3, action: enterConstant },
+  ],
+  [
+    { value: '/', action: binaryOp },
+    { value: 'C', action: clear },
+    { value: '0', action: enterConstant },
+    { value: '=', action: binaryOp },
+  ],
+];
